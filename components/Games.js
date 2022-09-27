5669bad08acd18b22b03fd601ffea395c6a9c40e
ftosse <felipeat95gmail.com>
2022-09-26 21:14:18 -0500

    retorno game.js

--- components/Games.js
+++ components/Games.js
@@ -1,20 +1,33 @@
-import api from '../api';
+import api, { getTopGames } from '../api';
 import React from 'react';
 import { useEffect, useState } from "react";
+import LiveChannelItem from './LiveChannelItem';
 
-function Games () {
+//import { getSession , signIn, signOut } from "next-auth/react";
+
+function Games() {
     const [games, setGames] = useState([]);
 
-    useEffect(() =>{
-        const fetchData = async () => {
-            const result = await api.get('https://api.twitch.tv/helix/games/top')
-            console.log(result.data);
-        };
-        fetchData()
-    });
-    return <div>
-        <h1> TOP GAMES </h1>
-    </div>
+
+    useEffect(() => {
+        getTopGames().then( response => {
+            //alert(JSON.stringify(response))
+            setGames(response.data.data)
+        }).catch(error => console.log(error))
+    },[])
+    
+    return <div className='grid sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 gap-4 py-2'>    
+        {games && (
+            games.map( game => 
+                <LiveChannelItem
+                img={game.box_art_url.replace('{width}',455).replace('{height}',455)}
+                profile_img={game.box_art_url}
+                title={game.name}
+                game={game.name}
+                />     
+            )
+        )}    
+      </div>
 };
 
 
--- components/LiveChannelItem.jsx
+++ components/LiveChannelItem.jsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 import React from 'react';
 
-const LiveChannelItem = ({ img, profile_img, title, user, game }) => {
+const LiveChannelItem = ({ img, profile_img, title, game }) => {
   return (
     <div className='p-2 w-full '>
-      <Image src={img} alt='/' width='555' height='315' />
+      <Image src={img} alt='/' width='355' height='515' />
       <div className='flex pt-2'>
         <div className='pr-2'>
           <Image
@@ -17,7 +17,6 @@ const LiveChannelItem = ({ img, profile_img, title, user, game }) => {
         </div>
         <div>
           <p className='text-sm font-bold'>{title}</p>
-          <p className='text-sm text-gray-500'>{user}</p>
           <p className='text-sm text-gray-500'>{game}</p>
         </div>
       </div>
